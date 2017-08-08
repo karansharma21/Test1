ef40a9a09d1d12137849c24f5fc15e91bb7e598c
karansharma21 <sharmakaran1244@gmail.com>
2017-08-09 01:23:48 +0530

    Changes in Code for Better Output

--- PycharmProjects/Test1/Guess.py
+++ PycharmProjects/Test1/Guess.py
@@ -1,19 +1,27 @@
 import random
+
 print 'Welcome TO Play Guessing Game'
 spy_name = raw_input('Enter your Name: ')
-num = random.randint(1,9)
+num = random.randint(1,8)
+count_win = 0
+count_loose = 0
 choice = 'Y'
-while(choice != 'N'):
+while (choice != 'N'):
     a = int(raw_input(str(spy_name) + " " + 'enter Your Number From 1 to 9:   '))
 
-    if(a<num and a>0 and a<10):
-        print str(spy_name) +" "+'Your Guess is Low:    '+str(a)
-    elif(a>num and a>0 and a<10):
-        print str(spy_name) +" "+'Your Guess is High:   '+str(a)
-    elif(a== num and a>0 and a<10):
-        print str(spy_name) +" "+'Your Guess is Perfect:    '+str(a)
+    if (a < num and a > 0 and a < 10):
+        print str(spy_name) + " " + 'Your Guess is Low:    ' + str(a)
+        count_loose += 1
+    elif (a > num and a > 0 and a < 10):
+        print str(spy_name) + " " + 'Your Guess is High:   ' + str(a)
+        count_loose += 1
+    elif (a == num and a > 0 and a < 10):
+        print str(spy_name) + " " + 'Your Guess is Perfect:    ' + str(a)
+        count_win += 1
     else:
-        print 'Wrong Input Given:   '+str(a)
-    print 'The original Number was: ' +str(num)
-    choice =raw_input( 'Do You want to Guess More? Y or N:   ')
+        print 'Wrong Input Given:   ' + str(a)
 
+    print spy_name + " Your number of Perfect Guess: " + str(count_win)
+    print spy_name + " Your number of non perfect Guess: " + str(count_loose)
+    print 'The original Number was: ' + str(num)
+    choice = raw_input('Do You want to Guess More? Y or N:   ')
--- PycharmProjects/Test1/stonepaperscissor.py
+++ PycharmProjects/Test1/stonepaperscissor.py
@@ -1,20 +1,28 @@
 print 'Welcome TO Play Stone Paper Scissor Game'
 spy_name1 = raw_input('Player 1, Enter your Name: ')
 spy_name2 = raw_input('Player 2, Enter your Name: ')
+count_win1=0
+count_win2=0
 choice = 'Y'
 while(choice != 'N'):
     a = str(raw_input(spy_name1 + " " + 'enter Your choice From Stone, Paper or Scissor:   '))
     b = str(raw_input(spy_name2 + " " + 'enter Your choice From Stone, Paper or Scissor:   '))
     if(str(a) == 'Stone' and str(b) == 'Scissor'):
         print 'Hahahaha' + " "  + str(spy_name1) + " " + 'wins'
+        count_win1 +=1
     elif str(a) == 'Paper'and str(b) == 'Stone':
         print 'Hahahaha' + " "  + str(spy_name1) + " " + 'wins'
+        count_win1 += 1
     elif (str(a) == 'Scissor'and str(b) == 'Paper'):
         print 'Hahahaha' + " " + str(spy_name1) + " " + 'wins'
+        count_win1 += 1
     elif (str(a) == str(b)):
         print 'Draw/Tie Game'
     else:
         print 'Hahahaha' + " " + str(spy_name2) + " " + 'wins'
+        count_win2 += 1
+    print spy_name1  + " : " + str(count_win1)
+    print spy_name2 + " : " + str(count_win2)
     choice =raw_input( 'Do You want to play More? Y or N:   ')
 
 
